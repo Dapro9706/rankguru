d5b5f780585a7cf9d4bc0a8cf11997d0531bc1ab
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-09-15 18:16:47 +0530

    json protocol bug fix

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-from .errors import AuthError
+from .errors import *
 from .utils import *
 from .globals import PLAIN_PY_PROTOCOL, JSON_PROTOCOL, PLACE_HOLDER
 
@@ -8,7 +8,7 @@ def hand(func):
         if self.protocol == JSON_PROTOCOL:
             try:
                 r = func (self, *args, **kwargs)
-                return {'STATUS':'SUCESS','PAYLOAD':r}
+                return {'STATUS': 'SUCESS', 'PAYLOAD': r}
             except Exception as e:
                 return {
                     'STATUS': 'ERROR',
@@ -29,26 +29,35 @@ class RG:
             raise AuthError ()
 
         self.protocol = protocol
-        self.NOVA_SERVICE_URL = f'https://rest.rankguru.com/tests?textbook={PLACE_HOLDER}'
+        self.NOVA_SERVICE_URL = f'https://rest.rankguru.com/'
+        self.TESTS = f'tests?textbook={PLACE_HOLDER}'
 
         self.API_URL = 'https://api.rankguru.com/graphql'
         self.ANS_QUERY = '{ QuestionEvaluation(input: {questionPaperId: "' + PLACE_HOLDER + '" })' \
-                                                       ' { evaluatedData { questionNo  key }  } }'
+                                                                                            ' { evaluatedData { questionNo  key }  } }'
 
     @hand
     def get_ans(self, QUESTION_PAPER_ID):
-        r = self.get_ans_raw(QUESTION_PAPER_ID)['data']['QuestionEvaluation']['evaluatedData']
+        r = self.get_ans_raw (QUESTION_PAPER_ID)
+        if self.protocol == JSON_PROTOCOL:
+            r = r['PAYLOAD']
 
-        ret = {}
-        for i in r:
-            ret[int(i['questionNo'])] = " ".join([chr(ord(i)+48) for i in i['key']])
-        return ret
+        r = r['data']['QuestionEvaluation']
 
+        if r:
+            r = r['evaluatedData']
+            ret = {}
+            for i in r:
+                ret[int (i['questionNo'])] = " ".join ([chr (ord (i) + 48) for i in i['key']])
+            return ret
+        else:
+            raise QPidError
 
+    @hand
     def get_ans_raw(self, QUESTION_PAPER_ID):
         r = make_post_request (self.HEADER, self.API_URL, com (self.ANS_QUERY, QUESTION_PAPER_ID))
+
         if r.status_code == 401:
             raise AuthError
         else:
-            return r.json()
-
+            return r.json ()
--- errors.py
+++ errors.py
@@ -1,3 +1,11 @@
 class AuthError (Exception):
     def __init__(self):
         super ().__init__ ('The given Auth has expired or is invalid')
+
+class QPidError (Exception):
+    def __init__(self):
+        super ().__init__ ('The given Question Paper ID is invalid')
+
+class TBidError (Exception):
+    def __init__(self):
+        super ().__init__ ('The given Text Book ID is invalid')
