672a0f5786b381b50ccfd3e237d3cf2476971b62
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-09-15 19:46:48 +0530

    pypi upload base

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ setup(
   packages = ['rankguru'],
   version = '0.12',
   license='MIT',
-  description = 'This is a basic Wrapper for Test answer retrieval from https://rankguru.com',   # Give a short description about your library
-  author = 'Swaminath Shiju',                   # Type in your name
-  author_email = 'swaminathshhiju@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/SwaminathShiju/rankguru',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  description = 'This is a basic Wrapper for Test answer retrieval from https://rankguru.com',
+  author = 'Swaminath Shiju',
+  author_email = 'swaminathshhiju@gmail.com',
+  url = 'https://github.com/SwaminathShiju/rankguru',
+  download_url = 'https://github.com/SwaminathShiju/rankguru/archive/refs/tags/v0.12.tar.gz',
   keywords = ['Basic'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.8',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.7'
   ],
 )
