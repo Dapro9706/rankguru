dd44f09d96aec9bd6c15a0bb10543f7f86ed0fb7
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-09-18 08:40:56 +0530

    conf rtd

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,7 @@ import os
 import sys
 
 sys.path.insert(1, os.path.abspath('..'))        
-
-
-DEBUG = not True
+DEBUG=False
 
 project = 'rankguru'
 copyright = '2021, Swaminath Shiju'
@@ -37,8 +35,41 @@ release = '0.12'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+if DEBUG:
+    extensions = [
+        'sphinx.ext.duration',
+        'sphinx.ext.doctest',
+        'sphinx.ext.autodoc',
+        'sphinx.ext.autosummary',
+        'sphinx.ext.intersphinx',
+    ]
 
-extensions = [
+    # Add any paths that contain templates here, relative to this directory.
+    templates_path = ['_templates']
+
+    # List of patterns, relative to source directory, that match files and
+    # directories to ignore when looking for source files.
+    # This pattern also affects html_static_path and html_extra_path.
+    exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+    # -- Options for HTML output -------------------------------------------------
+
+    # The theme to use for HTML and HTML Help pages.  See the documentation for
+    # a list of builtin themes.
+    #
+
+
+    # html_theme = 'alabaster' if DEBUG else 'sphinx-rtd-theme'
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = ["_themes", ]
+
+    # Add any paths that contain custom static files (such as style sheets) here,
+    # relative to this directory. They are copied after the builtin static files,
+    # so a file named "default.css" will overwrite the builtin "default.css".
+    html_static_path = ['_static']
+else:
+    extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
@@ -46,27 +77,14 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+    intersphinx_mapping = {
+        'python': ('https://docs.python.org/3/', None),
+        'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    }
+    intersphinx_disabled_domains = ['std']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+    templates_path = ['_templates']
 
+    # -- Options for HTML output
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-
-
-# html_theme = 'alabaster' if DEBUG else 'sphinx-rtd-theme'
-html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+    html_theme = 'sphinx_rtd_theme'
