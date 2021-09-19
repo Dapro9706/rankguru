6d84af9b6c8b896807863d4bfe2f5d0ac376c7c4
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-09-19 15:20:49 +0530

    1.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'rankguru',
   packages = ['rankguru'],
-  version = '0.5',
+  version = '1.1',
   license='MIT',
   description = 'This is a basic Wrapper for Test answer retrieval from https://rankguru.com',
   author = 'Swaminath Shiju',
   author_email = 'swaminathshhiju@gmail.com',
   url = 'https://github.com/SwaminathShiju/rankguru',
-  download_url = 'https://github.com/SwaminathShiju/rankguru/archive/refs/tags/v0.5.tar.gz',
+  download_url = 'https://github.com/SwaminathShiju/rankguru/archive/refs/tags/v1.1.tar.gz',
   keywords = ['Basic'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
